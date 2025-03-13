987131abf4a411b8958608bbb67617674463b61a
amardeep-SDE <amardeepdwivedi1494@gmail.com>
2025-03-13 23:05:39 +0530

    fix: user details

--- src/User.tsx
+++ src/User.tsx
@@ -20,7 +20,7 @@ const Info = {
     "React Developer",
     "MERN Stack Developer",
   ],
-  bio: "Software Development Engineer with 1 year and 9 months of ongoing experience in building responsive web applications using React and MERN stack, with expertise in scalable full stack solutions. Skilled in JavaScript, TypeScript, and frameworks like Tailwind CSS, Material UI and Express.js, with MongoDB database. Experienced in Redux Toolkit for state management and code optimization, GitHub, and Jira. Focused on delivering scalable solutions and ensuring client satisfaction through timely project delivery. As a developer I'm eager to contribute to all aspects of development. A quick learner, I adapt to new technologies and stay ahead of industry trends.",
+  bio: "Software Development Engineer with 2+ years of experience in building responsive web applications using React and MERN stack, with expertise in scalable full stack solutions. Skilled in JavaScript, TypeScript, and frameworks like Tailwind CSS, Material UI and Express.js, with MongoDB database. Experienced in Redux Toolkit for state management and code optimization, GitHub, and Jira. Focused on delivering scalable solutions and ensuring client satisfaction through timely project delivery. As a developer I'm eager to contribute to all aspects of development. A quick learner, I adapt to new technologies and stay ahead of industry trends.",
 };
 
 const ProjectInfo = [
